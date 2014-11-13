bad177711bf333085e186d6ebae513184c53555c
Matthew <Matthew@MattPC>
2014-11-13 16:23:56 -0600

    Starting up lab3 now. Lab2 came out very well.

---
+++ src/lab3/DemoStartup.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3;
+
+import java.util.*;
+
+/**
+ *
+ * @author Matthew
+ */
+public class DemoStartup {
+    public static void main(String[] args) {
+        Song song1 = new Song("1", "First Song Ever", "Nick Cage", "Rock");
+        Song song2 = new Song("2", "Second Song", "Bill Clinton", "Jazz");
+        Song song3 = new Song("3", "Third Song", "Nobody Important", "Classical");
+        Song song4 = new Song("4", "Fourth Song", "Justin Case", "Pop");
+        Song song5 = new Song("1", "First Song Ever", "Nick Cage", "Rock");
+        
+        List test = new ArrayList();
+            test.add(song1);
+            test.add(song2);
+            test.add(song3);
+            test.add(song4);
+            test.add(song5);
+        
+        for(int i = 0; i < test.size(); i++){
+            System.out.println(test.get(i));
+        }
+        
+        if(song1.getId().equals(song5.getId())){
+            System.out.println("They are equal in this test!");
+        }else{
+            System.out.println("This test Failed!");
+        }
+        System.out.println("End of list tests for testing purposes!");
+        System.out.println("---------------------------------------");
+        System.out.println("STARTING LAB2 SPECIFICATIONS NOW.......");
+        Map<Integer,Song> songs = new LinkedHashMap<>();
+        songs.put(1, song1);
+        songs.put(2, song2);
+        songs.put(3, song3);
+        songs.put(4, song4);
+        songs.put(5, song5);
+        
+        for(int p = 1; p <= songs.size(); p++){
+            System.out.println(songs.get(p));
+        }
+    }
+}
---
+++ src/lab3/Song.java
@@ -0,0 +1,87 @@
+package lab3;
+
+import java.util.Objects;
+
+public class Song {
+    private String id;
+    private String songName;
+    private String authName;
+    private String genre;
+    
+    public String getSongInfo(int id, String songName, String authName, String genre){
+        return id + " Name: " + songName + " Author: " + authName + " Genre: " + genre;
+    }
+    
+    public Song(String id, String songName, String authName, String genre){
+        this.id = id;
+        this.songName = songName;
+        this.authName = authName;
+        this.genre = genre;
+    }
+    
+    private String generateId(String id){
+        return id;
+    }
+    
+    private String retrieveSongName(String songName){
+        return songName;
+    }
+    
+    private String retrieveAuthorName(String authName){
+        return authName;
+    }
+    
+    private String genre(String genre){
+        return genre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getAuthName() {
+        return authName;
+    }
+
+    public void setAuthName(String authName) {
+        this.authName = authName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(id); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        return hash;    
+    }
+    
+    @Override
+    public String toString() {
+        return "ID: " + id + " Song Name: " + songName + " Author Name: " + authName + " Genre: " + genre ;
+    }
+    
+}
